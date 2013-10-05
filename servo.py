55a516869d79cbd4a429b04a4329736237132aa0
Carl Lipo <clipo@csulb.edu>
2013-10-05 13:34:30 -0700

    initial submission

--- servo.py
+++ servo.py
@@ -3,15 +3,15 @@ __author__ = 'clipo'
 # Servo Control
 import time
 def set(property, value):
- try:
- f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
- f.write(value)
- f.close()
- except:
- print("Error writing to: " + property + " value: " + value)
-def setServo(angle):
- set("servo", str(angle))
+    try:
+        f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+        f.write(value)
+        f.close()
+    except:
+        print("Error writing to: " + property + " value: " + value)
 
+def setServo(angle):
+    set("servo", str(angle))
 
 set("delayed", "0")
 set("mode", "servo")
@@ -19,9 +19,9 @@ set("servo_max", "180")
 set("active", "1")
 delay_period = 0.01
 while True:
- for angle in range(0, 180):
- setServo(angle)
- time.sleep(delay_period)
- for angle in range(0, 180):
- setServo(180 - angle)
- time.sleep(delay_period)
+    for angle in range(0, 180):
+        setServo(angle)
+        time.sleep(delay_period)
+    for angle in range(0, 180):
+        setServo(180 - angle)
+        time.sleep(delay_period)
