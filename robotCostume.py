324449d5b317b170427de3f92318337f89d31f2f
Carl Lipo <clipo@csulb.edu>
2013-10-08 04:18:55 +0000

    ifunctional version wq

--- robotCostume.py
+++ robotCostume.py
@@ -7,9 +7,12 @@ import time
 import random
 from subprocess import call
 import getch
+import os
 
 led = LPD8806.strand()
 GPIO.setmode(GPIO.BCM)
+os.system("amixer set PCM -- -2200")
+os.system("amixer cset numid=3 1")
 
 directory = "./sounds/"
 sounds=["affirmative2.wav",
@@ -22,7 +25,7 @@ sounds=["affirmative2.wav",
 
 # GPIO 23 set up as input. It is pulled up to stop false signals
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -36,44 +39,43 @@ def quit():
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
 
 # this will run in another thread when our event is detected
-def button1(channel):
+def button1():
     print "Beep 1\n"
-    rand=random.randrange(0,5)
-    alist=["happy.wav","happyhalloween.wav","merrychristmas.wav","candycorn.wav","greatpumpkin.wav"]	
-    file="./sounds/"+alist[rand]
-    call(file)
+    alist=["happyhalloween.wav","merrychristmas.wav","candycorn.wav","greatpumpkin.wav"]	
+    rand=random.randrange(0,len(alist))
+    file="aplay ./sounds/"+alist[rand]
+    os.system(file)
 
 # this will run in another thread when our event is detected
-def button2(channel):
+def button2():
     print "Beep 2\n"
-    rand=random.randrange(0,9)
     blist=["beepboopboop2.wav","blip2.wav","lowbeepboop.wav","beepboopboopboop.wav","blip.wav","beepbeepbeepbeep.wav","beepbeep.wav","beep.wav","countdwn.wav"]	
-    file="./sounds/"+blist[rand]
-    call(file)
+    rand=random.randrange(0,len(blist))
+    file="aplay ./sounds/"+blist[rand]
+    os.system(file)
 
 # this will run in another thread when our event is detected
-def button3(channel):
+def button3():
     print "Beep 3\n"
-    rand=random.randrange(0,10)
     clist=["affirmative2.wav","error.wav","hello.wav","imrobot2.wav","robotcom.wav","affirmative.wav","iamrobot.wav","iamrobot3.wav","robots.wav","iamrobot5.wav"]	
-    file="./sounds/"+clist[rand]
-    call(file)
-
+    rand=random.randrange(0,len(clist))
+    file="aplay ./sounds/"+clist[rand]
+    os.system(file)
 
 # this will run in another thread when our event is detected
-def button4(channel):
+def button4():
     print "Beep 4\n"
-    rand=random.randrange(0,len(list))
-    file="./sounds/"+list[rand]
-    call(file)
+    rand=random.randrange(0,len(sounds))
+    file="aplay ./sounds/"+sounds[rand]
+    os.system(file)
 
 # this will run in another thread when our event is detected
-def button5(channel):
+def button5():
     print "Beep 5\n"
-    rand=random.randrange(0,4)
     dlist=["trortr.wav","trickortreat3.wav","trickortreat.wav","gimmesomecandy.wav"]	
-    file="./sounds/"+dlist[rand]
-    call(file)
+    rand=random.randrange(0,len(dlist))
+    file="aplay ./sounds/"+dlist[rand]
+    os.system(file)
 
     if stateValue == 0:
         for angle in range(0, 180):
@@ -100,23 +102,14 @@ def setServo(angle):
 # else is happening in the program, the function "my_callback" will be run
 # It will happen even while the program is waiting for
 # a falling edge on the other button.
-GPIO.add_event_detect(20, GPIO.FALLING, callback=lambda x: button1(), bouncetime=2000)
-GPIO.add_event_detect(21, GPIO.FALLING, callback=lambda x: button2(), bouncetime=2000)
-GPIO.add_event_detect(22, GPIO.FALLING, callback=lambda x: button3(), bouncetime=2000)
-GPIO.add_event_detect(23, GPIO.FALLING, callback=lambda x: button4(), bouncetime=2000)
-GPIO.add_event_detect(24, GPIO.FALLING, callback=lambda x: button5(), bouncetime=2000)
-
-def keycheck():
-    ch = getch.getch()
-    if ch == '\x03':
-        quit()
-    elif ch=='q':
-        quit()
+#GPIO.add_event_detect(17, GPIO.FALLING, callback=lambda x: button1(), bouncetime=2000)
+#GPIO.add_event_detect(20, GPIO.FALLING, callback=lambda x: button2(), bouncetime=2000)
+#GPIO.add_event_detect(22, GPIO.FALLING, callback=lambda x: button3(), bouncetime=2000)
+GPIO.add_event_detect(23, GPIO.FALLING, callback=lambda x: button4(), bouncetime=500)
+#GPIO.add_event_detect(24, GPIO.FALLING, callback=lambda x: button5(), bouncetime=2000)
 
 while True:
-    keycheck()
     for i in range(5):
-        keycheck()
         led.fill(255, 0, 0)
         led.update()
         sleep(0.3)
@@ -126,10 +119,7 @@ while True:
         led.fill(0, 0, 255)
         led.update()
         sleep(0.3)
-    keycheck()
     for i in range(300):
-        keycheck()
         led.wheel()
         led.update()
-    keycheck()
 
